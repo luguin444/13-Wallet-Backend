a57349a7a8ae4d487ac9ab267d041e3d221adaf5
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2020-12-04 19:08:20 -0300

    Deploy Back com database

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 
+const dbConfig = { connectionString: process.env.DATABASE_URL };
+const connection = new Pool(dbConfig);
 
-const connection = new Pool( {
-    "host": "localhost",
-    "user": "postgres",
-    "password": "147",
-    "database": "wallet",
-    "port": 5432
-})
+//Uso local
+// const connection = new Pool( {
+//     "host": "localhost",
+//     "user": "postgres",
+//     "password": "147",
+//     "database": "wallet",
+//     "port": 5432
+// })
 
 module.exports = connection;
